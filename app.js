0f233c46912453cd6c3899c392b8ae8b2d8b2421
sirraghavgupta <raghav.mhc@gmail.com>
2019-12-02 07:23:49 -0500

    version 2.1 data persistence added

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
-var express = require("express");
-var app = express();
+var express    = require("express"),
+	app        = express(),
+	bodyParser = require("body-parser"),
+	mongoose   = require('mongoose');
 
-// var rp = require("request-promise");
+mongoose.connect("mongodb://localhost/yelp_camp",{
+	useNewUrlParser:true,
+	useFindAndModify:false,
+	useCreateIndex:true,
+	useUnifiedTopology:true
+});
 
-var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.set("view engine", "ejs");
 
-var campgrounds = [
-		{name: "raghav", image:"http://vivalifestyleandtravel.com/images/cache/c-1509451562-2093217504.jpg"},
-		{name: "mohit", image:"https://www.tripsavvy.com/thmb/mU3xd61g1i_9VaZEm9I4_-lyfps=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/camping-under-the-stary-sky-on-mountain-805170924-5986334fd088c0001167b241.jpg"},
-		{name: "gunjan", image:"https://cdn.hiconsumption.com/wp-content/uploads/2015/06/Best-Places-To-Camp-In-The-US-0-Hero.jpg"},
-		{name: "didu", image:"https://img.theculturetrip.com/768x432/wp-content/uploads/2018/02/best-camping-sites-near-pune-india.jpg"},
-		{name: "raghav", image:"http://vivalifestyleandtravel.com/images/cache/c-1509451562-2093217504.jpg"},
-		{name: "mohit", image:"https://www.tripsavvy.com/thmb/mU3xd61g1i_9VaZEm9I4_-lyfps=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/camping-under-the-stary-sky-on-mountain-805170924-5986334fd088c0001167b241.jpg"},
-		{name: "gunjan", image:"https://cdn.hiconsumption.com/wp-content/uploads/2015/06/Best-Places-To-Camp-In-The-US-0-Hero.jpg"},
-		{name: "didu", image:"https://img.theculturetrip.com/768x432/wp-content/uploads/2018/02/best-camping-sites-near-pune-india.jpg"},
-		{name: "raghav", image:"http://vivalifestyleandtravel.com/images/cache/c-1509451562-2093217504.jpg"},
-		{name: "mohit", image:"https://www.tripsavvy.com/thmb/mU3xd61g1i_9VaZEm9I4_-lyfps=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/camping-under-the-stary-sky-on-mountain-805170924-5986334fd088c0001167b241.jpg"},
-		{name: "gunjan", image:"https://cdn.hiconsumption.com/wp-content/uploads/2015/06/Best-Places-To-Camp-In-The-US-0-Hero.jpg"},
-		{name: "didu", image:"https://img.theculturetrip.com/768x432/wp-content/uploads/2018/02/best-camping-sites-near-pune-india.jpg"}
+//SCHEMA FOR THE CAMPGROUND
+var campgroundSchema = new mongoose.Schema({
+	name : String,
+	image: String
+});
 
-];
+var Campground = mongoose.model("Campground", campgroundSchema);
 
+// INSERT A CAMPGROUND
+/*Campground.create({
+	name: "raghav", 
+	image:"http://vivalifestyleandtravel.com/images/cache/c-1509451562-2093217504.jpg"
+	},
+	function(err, campground){
+		if(err)
+			console.log(err);
+		else{
+			console.log("OUR NEWLY INSERTED CAMPGROUND");
+			console.log(campground);
+		}
+	});
+*/
 app.get("/", function(req, res){
+
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-	// res.send("raghav is awesome");
-	res.render("campgrounds", {campgrounds: campgrounds});
+	// get all campgrounds from the DB
+	Campground.find({}, function(err,allCampgrounds){
+		if(err)
+			console.log(err);
+		else{
+			res.render("campgrounds", {campgrounds: allCampgrounds});	
+		}
+	});
 });
 
 app.post("/campgrounds", function(req, res){
@@ -38,9 +56,13 @@ app.post("/campgrounds", function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-
-	res.redirect("/campgrounds");
+	
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err)
+			console.log(err);
+		else
+			res.redirect("/campgrounds");
+	});
 });
 
 // study the REST convention pls --
